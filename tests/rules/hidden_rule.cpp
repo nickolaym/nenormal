8d7a3efe0ec9dafa8511a6b33e0f6494737abea9
Nikolay Merkin <merkin@mail.ru>
2026-06-06 18:36:19 +0300

    fixup - hidden rule

--- tests/rules/hidden_rule.cpp
+++ tests/rules/hidden_rule.cpp
@@ -1,5 +1,6 @@
 #include "nenormal/nenormal.h"
 #include <gtest/gtest.h>
+#include "./utils.h"
 
 namespace nn { namespace {
 
@@ -19,6 +20,27 @@ TEST(hidden_rule, mismatched) {
 
     static_assert(p(not_matched_yet{at}) == not_matched_yet{augmented_text{t, passed{0}}});
     static_assert(m(at) == augmented_text{t, passed{0}});
+
+    // inplace
+
+    static_assert(call_inplace_ex(p, std::string(t.value.view()))
+        == inplace_argument{std::string(t.value.view()), tristate_kind::not_matched_yet}
+    );
+    static_assert(
+        call_inplace_ex(p,
+            inplace_augmented_text{
+                std::string(t.value.view()),
+                inplace_cumulative_effect{0, inc}
+            }
+        ) ==
+        inplace_argument{
+            inplace_augmented_text{
+                std::string(t.value.view()),
+                inplace_passed{0}
+            },
+            tristate_kind::not_matched_yet
+        }
+    );
 }
 
 TEST(hidden_rule, regular) {
@@ -33,6 +55,27 @@ TEST(hidden_rule, regular) {
 
     static_assert(p(not_matched_yet{at}) == matched_regular{augmented_text{t_one, passed{0}}});
     static_assert(m(at) == augmented_text{t_all, passed{0}});
+
+    // inplace
+
+    static_assert(call_inplace_ex(p, std::string(t.value.view()))
+        == inplace_argument{std::string(t_one.value.view()), tristate_kind::matched_regular}
+    );
+    static_assert(
+        call_inplace_ex(p,
+            inplace_augmented_text{
+                std::string(t.value.view()),
+                inplace_cumulative_effect{0, inc}
+            }
+        ) ==
+        inplace_argument{
+            inplace_augmented_text{
+                std::string(t_one.value.view()),
+                inplace_passed{0}
+            },
+            tristate_kind::matched_regular
+        }
+    );
 }
 
 TEST(hidden_rule, final) {
@@ -45,6 +88,27 @@ TEST(hidden_rule, final) {
 
     static_assert(p(not_matched_yet{at}) == matched_final{augmented_text{t_one, passed{0}}});
     static_assert(m(at) == augmented_text{t_one, passed{0}});
+
+    // inplace
+
+    static_assert(call_inplace_ex(p, std::string(t.value.view()))
+        == inplace_argument{std::string(t_one.value.view()), tristate_kind::matched_final}
+    );
+    static_assert(
+        call_inplace_ex(p,
+            inplace_augmented_text{
+                std::string(t.value.view()),
+                inplace_cumulative_effect{0, inc}
+            }
+        ) ==
+        inplace_argument{
+            inplace_augmented_text{
+                std::string(t_one.value.view()),
+                inplace_passed{0}
+            },
+            tristate_kind::matched_final
+        }
+    );
 }
 
 
